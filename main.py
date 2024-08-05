65e859a78872b3aea74db53d5e9fb00f098b0365
Mohammad Saddam Ali <mohdsaddamali002@gmail.com>
2024-08-05 18:42:37 +0545

    Adding some variables and printing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-print("Hello world")
+name = "Mohammad saddam ali"
+age = 24
+weight = 60
 
-country = "Nepal"
 
-print(country)
+print(f"My name is {name}, my age is {age} and my weight is {weight}")
+
+
+
+
+
+
